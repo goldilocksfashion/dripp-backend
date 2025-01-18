16b2564989f142b40868f8c4a0622e0683c6d255
Anirudh Vyas <av@goldilocksfashion>
2025-01-18 10:15:02 -0800

    dripp backend

--- git_rewrite.sh
+++
@@ -1,8 +0,0 @@
-git filter-repo --email-callback '
-    if commit.author_email == b"anirudhvyas@Anirudhs-MacBook-Pro.local":
-        commit.author_email = b"ricky.nj@gmail.com"
-        return commit
-    if commit.committer_email == b"anirudhvyas@Anirudhs-MacBook-Pro.local":
-        commit.committer_email = b"ricky.nj@gmail.com"
-        return commit
-'
